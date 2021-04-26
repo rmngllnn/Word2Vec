518f0a7719e3198c7c40da2c6be7686bde8fadd9
Romy_R <romyrabemihanta@gmail.com>
2021-04-26 12:19:00 +0200

    Update init.py

--- init.py
+++ init.py
@@ -13,7 +13,7 @@ EMBEDDING_DIM = 10 #pour commencer
 tokens = []
 vocab = set(['D1','D2','F1','F2'])
 #TODO : remplir la liste du vocabulaire vocab avec le corpus, séparer en tokens et remplir tokens
-#TODO : avec une fonction, ouvrir le fichier donné par le parser, parcourir chaque ligne, supprimer les tags, gérer ou non les majuscules
+#TODO : avec une fonction, ouvrir le fichier donné par le parser, parcourir chaque ligne, supprimer les tags, gérer ou non les majuscules, attention à l'encodage (UTF-8)
 def fill_vocab(infile) :
   pass
 
