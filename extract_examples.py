32727b60cd8396e99cad6b4182b655dda9f6f96a
CGuitel <cecile.guitel@gmail.com>
2021-06-13 15:29:11 +0200

    ajout du subsampling des mots fréquents
    du coup ajout du paramètre sub_sampling, seuil de fréquence pour appliquer le subsampling. Attention, passé les autres debugs en commentaire pour l'instant

--- extract_examples.py
+++ extract_examples.py
@@ -12,10 +12,11 @@ import numpy as np
 import argparse
 import random as rd
 import time
+from math import sqrt
 
 class ExampleCorpus:
     """ Corpus of examples, created based on a serialized tokenized doc and the given parameters.
-    Positive examples from the doc, negative examples created using negative sampling.
+    Positive examples from the doc, negative examples created using negative neg_sampling.
 
     verbose             bool, verbose mode
     debug               bool, debug mode
@@ -28,7 +29,9 @@ class ExampleCorpus:
                         if = 2, then for each word, two negative examples are randomly created:
                         (r1, x, -), (r2, x, -)
     min_occurences      int, the minimum number of occurences needed for a word to be learned
-    sampling            float, sampling rate to calculate the negative example distribution probability
+    sub_sampling        float, frequency threshold above which words are considered frequent and
+                        subsampled
+    neg_sampling        float, sampling rate to calculate the negative example distribution
 
     examples            list of tuples, examples[(int)] = (context_word_id, target_word_id, pos|neg)
     i2w                 list of ints, index to word translator, i2w[index(int)] = "word"
@@ -39,7 +42,8 @@ class ExampleCorpus:
             context_size,
             max_vocab_size,
             min_occurences,
-            sampling,
+            sub_sampling,
+            neg_sampling,
             number_neg_examples,
             debug,
             verbose):
@@ -62,19 +66,23 @@ class ExampleCorpus:
         assert type(min_occurences) is int, "Problem with max_vocab_size."
         self.min_occurences = min_occurences
 
-        assert type(sampling) is float and sampling > 0 and sampling < 1, "Problem with sampling."
-        self.sampling = sampling
+        assert type(sub_sampling) is float and sub_sampling > 0 and sub_sampling < 1, "Problem with sub_sampling."
+        self.sub_sampling = sub_sampling
+
+        assert type(neg_sampling) is float and neg_sampling > 0 and neg_sampling < 1, "Problem with neg_sampling."
+        self.neg_sampling = neg_sampling
 
         assert type(number_neg_examples) is int and number_neg_examples > 0, "Problem with number_neg_examples."
         self.number_neg_examples = number_neg_examples
 
         if self.verbose:
-            print("\Started example extraction.")
+            print("\nStarted example extraction.")
             print("\nParameters:")
             print("context size = " + str(self.context_size))
             print("max vocabulary size = " + str(self.max_vocab_size))
             print("min occurences to learn an embedding = "+str(self.min_occurences))
-            print("sampling rate = " + str(self.sampling))
+            print("sub sampling rate = " + str(self.sub_sampling))
+            print("neg sampling rate = " + str(self.neg_sampling))
             print("negative examples per positive example = " + str(self.number_neg_examples))
         
         self._occurence_counter = self.__get_occurence_counter()
@@ -102,21 +110,36 @@ class ExampleCorpus:
         of embeddings learned, an only learn max_vocab_size - 2*self.context_size - 1 real word
         embeddings.
         """
-        occurence_counter = Counter() # We want to count the number of occurences of each token, to only keep the max_vocab_size most common ones.
-
+        occurence_counter = Counter() # We want to count the number of occurences of each token, then
+        # filter the tokens down.
         for sentence in self._tokenized_doc:
             occurence_counter.update(sentence)
 
         UNK_counter = Counter()
 
+        # Filtering for maximum vocab size:
         if self.max_vocab_size > 0 and self.max_vocab_size < len(occurence_counter.keys()): # If a maximum
             # vocab size has been set, and we're over it...
             UNK_counter.update({token : count for (token, count)
                     in occurence_counter.most_common()[self.max_vocab_size:]})
 
+        # Filtering for mininum number of occurences:
         if self.min_occurences > 0: # if a minimum occurence count has been set...
             UNK_counter.update({token : count for (token, count) in occurence_counter.items()
-                    if count >= self.min_occurences})  
+                    if count >= self.min_occurences})
+
+        # Subsampling frequent words:
+        if self.verbose: print("\nSubsampling frequent words.")
+        rng = np.random.default_rng(12345)
+        total_occurences = sum([occurence_counter[word] for word in occurence_counter])
+        for word in occurence_counter:
+            if occurence_counter[word]/total_occurences > self.sub_sampling: # TODO Depending on the source consulted, subsampling is applied to all or only frequent words...?
+                proba = 1 - sqrt(self.sub_sampling/occurence_counter[word]) # probability of not keeping
+                # that word.
+                random = rng.random() #  We pick a float number between 0 and 1.
+                if random <= proba: # We have (proba) chance of picking a lower number and not keeping the word.
+                    if self.debug: print("deleting frequent word: "+word)
+                    UNK_counter[word] = occurence_counter[word]
         
         occurence_counter.subtract(UNK_counter) # All those other tokens are deleted from the occurence count...
         occurence_counter.update({"UNK": sum([UNK_counter[token] for token in UNK_counter])}) # and counted as occurences of UNK.
@@ -126,7 +149,7 @@ class ExampleCorpus:
             + ["*F"+str(i)+"*" for i in range(1,self.context_size+1)]}) # We add one count of each out-of-bound special word per sentence.
 
         if self.verbose: print("\nOccurence counter created.")
-        if self.debug: print("Occurence count: "+str(+occurence_counter))
+        # if self.debug: print("Occurence count: "+str(+occurence_counter))
         return +occurence_counter # "+" removes 0 or negative count elements.
 
 
@@ -160,17 +183,17 @@ class ExampleCorpus:
         """
         prob_dist = {}
 
-        total_word_count = sum([self._occurence_counter[word]**self.sampling for word in self._occurence_counter])
+        total_word_count = sum([self._occurence_counter[word]**self.neg_sampling for word in self._occurence_counter])
         for word in self._occurence_counter:
-            prob_dist[word] = (self._occurence_counter[word]**self.sampling)/total_word_count
+            prob_dist[word] = (self._occurence_counter[word]**self.neg_sampling)/total_word_count
 
         if self.verbose: print("\nProbability distribution created.")
-        if self.debug: print("Probability distribution: "+str(prob_dist))
+        # if self.debug: print("Probability distribution: "+str(prob_dist))
         return prob_dist
 
 
     def __create_examples(self):
-        """Creates positive and negative examples using negative sampling. The window size is also sampled
+        """Creates positive and negative examples using negative neg_sampling. The window size is also sampled
         between context_size and 1.
 
         An example is a (target word, context word, gold tag) tuple.
@@ -180,13 +203,13 @@ class ExampleCorpus:
         start_time = time.time()
 
         rng = np.random.default_rng(12345)
-        sampling_values = []
-        sampling_words = [word for word in self._prob_dist]
+        neg_sampling_values = []
+        neg_sampling_words = [word for word in self._prob_dist]
         current_total_proba = 0
         overall_total_proba = 100000
-        for word in sampling_words:
+        for word in neg_sampling_words:
             current_total_proba += self._prob_dist[word]*overall_total_proba
-            sampling_values.append(current_total_proba) # We create a list with the cummulative
+            neg_sampling_values.append(current_total_proba) # We create a list with the cummulative
             # probability of each word, so that each one takes more of less 'space' in the amount
             # based on its probability. We can then sample between 0 and the total, and each one has
             # (approximately) that probability of being picked.
@@ -196,19 +219,19 @@ class ExampleCorpus:
         for i, sentence in enumerate(self._indexed_doc): # For each sentence...
             if self.verbose and i % 100 == 0:
                 curr_time = time.time()
-                print("sentence ",i,"\t",len(examples),"\t", curr_time-start_time)
+                print("sentence ",i,"\tnumber examples ",len(examples),"\ttime ", curr_time-start_time)
             for target_i in range(self.context_size, len(sentence) - self.context_size): # For each word of the actual sentence...
-                    sampled_context_size = rd.randint(1, self.context_size) # We sample a context size.
+                    sampled_context_size = rng.integers(low=1, high=self.context_size) # We sample a context size.
                     for context_i in range(target_i - sampled_context_size, target_i + sampled_context_size + 1): # For each word in the (sampled) context window...
                         if target_i is not context_i:
                                 examples.append((sentence[target_i], sentence[context_i], 1))
-                                if self.debug: print(self.i2w[sentence[target_i]]+","+self.i2w[sentence[context_i]]+",1")
+                                # if self.debug: print(self.i2w[sentence[target_i]]+","+self.i2w[sentence[context_i]]+",1")
                         
-                    for neg_example in range(self.number_neg_examples): # Now, negative sampling! Using that probability distribution/sampler.
+                    for neg_example in range(self.number_neg_examples): # Now, negative neg_sampling! Using that probability distribution/sampler.
                         random_probability = rng.integers(low=0, high=overall_total_proba)
-                        neg_rank = np.searchsorted(sampling_values, random_probability)
-                        neg_id = self.w2i[sampling_words[neg_rank]]
-                        if self.debug: print(self.i2w[sentence[target_i]]+","+self.i2w[neg_id]+",0")
+                        neg_rank = np.searchsorted(neg_sampling_values, random_probability)
+                        neg_id = self.w2i[neg_sampling_words[neg_rank]]
+                        # if self.debug: print(self.i2w[sentence[target_i]]+","+self.i2w[neg_id]+",0")
                         examples.append((sentence[target_i], neg_id, 0))
         
         if self.verbose: print("\n"+str(len(examples))+" examples created.")
@@ -234,7 +257,8 @@ if __name__ == "__main__":
     parser.add_argument('--min_occurences', type=int, default=3, help='The minimum number of occurences needed for a word to be learned')
     parser.add_argument('--context_size', type=int, default=2, help='The size of the context window on each side of the target word')
     parser.add_argument('--number_neg_examples', type=int, default=3, help='The number of negative examples per positive example')
-    parser.add_argument('--sampling', type=float, default=0.75, help='The sampling rate to calculate the negative example distribution probability')
+    parser.add_argument('--sub_sampling', type=float, default=0.01, help='The frequency threshold above which words are considered frequent and subsampled')
+    parser.add_argument('--neg_sampling', type=float, default=0.75, help='The neg_sampling rate to calculate the negative example distribution probability')
     parser.add_argument('--verbose', type=bool, default=True, help='Verbose mode')
     parser.add_argument('--debug', type=bool, default=False, help='Debug mode')
     args = parser.parse_args()
@@ -243,7 +267,8 @@ if __name__ == "__main__":
 
     corpus = ExampleCorpus(tokenized_doc = tokenized_doc,
         context_size = args.context_size,
-        sampling = args.sampling,
+        neg_sampling = args.neg_sampling,
+        sub_sampling = args.sub_sampling,
         number_neg_examples = args.number_neg_examples,
         max_vocab_size = args.max_vocab_size,
         min_occurences = args.min_occurences,
